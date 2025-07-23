1723a6102eb7bbb02c16bf140cbe8911259781b0
beerendramc <beerendramc@gmail.com>
2025-07-23 15:57:33 +0530

    updated webpack.config.js to handle css and assets so that we can depend on webpack alone to bundle mfe

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,52 @@
 const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
-module.exports = {
-  mode: "production",
-  entry: {
-    bundle: [
-      "./dist/angular-mfe-demo/browser/polyfills.js",
-      "./dist/angular-mfe-demo/browser/main.js",
+module.exports = (env) => {
+  if (!env || !env.project_name) {
+    throw new Error(
+      "Error: 'project_name' environment variable is required. Please provide it using --env project_name=<your-project-name>"
+    );
+  }
+  const projectName = env.project_name;
+  const environment = env.target_env || "dev";
+
+  const srcPath = `./dist/${projectName}-${environment}/browser`;
+  const dstPath = `./elements/${projectName}-${environment}`;
+
+  console.log(`Building for Project: ${projectName}`);
+  console.log(`Target Environment: ${environment}`);
+  console.log(`Source Path: ${srcPath}`);
+  console.log(`Output Path: ${dstPath}`);
+
+  return {
+    mode: "production",
+    entry: {
+      bundle: [`${srcPath}/polyfills.js`, `${srcPath}/main.js`],
+    },
+    output: {
+      path: path.resolve(__dirname, dstPath), // Output directory
+      filename: `${projectName}-element.js`,
+    },
+    module: {
+      rules: [
+        // You might need rules for handling other file types (e.g., Babel for older browser support)
+      ],
+    },
+    plugins: [
+      new CopyWebpackPlugin({
+        patterns: [
+          {
+            from: `${srcPath}/styles.css`,
+            to: `${projectName}-styles.css`,
+          },
+          {
+            from: `${srcPath}/assets`,
+            to: "assets",
+            noErrorOnMissing: true, // Prevents errors if the assets directory does not exist
+          },
+        ],
+      }),
     ],
-  },
-  output: {
-    path: path.resolve(__dirname, "elements/angular-mfe-demo"), // Output directory
-    filename: "angular-mfe-demo-element.js",
-  },
-  module: {
-    rules: [
-      // You might need rules for handling other file types (e.g., Babel for older browser support)
-      // {
-      //   test: /\.js$/,
-      //   exclude: /node_modules/,
-      //   use: {
-      //     loader: 'babel-loader',
-      //     options: {
-      //       presets: ['@babel/preset-env'],
-      //     },
-      //   },
-      // },
-    ],
-  },
-  // devtool: 'inline-source-map' // Optional: for easier debugging
+    // devtool: 'inline-source-map' // Optional: for easier debugging
+  };
 };
