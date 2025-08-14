82eecf674b064d8bca5bc0a54a2f23e28bc530b1
Beerendra M C <beerendramc@gmail.com>
2025-08-14 14:07:52 +0530

    Update main.ts
    added dynamic logic to bootstrap app or build mfe

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,25 @@
-import { createApplication } from '@angular/platform-browser';
+import { bootstrapApplication, createApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { createCustomElement } from '@angular/elements';
 import { GreetingsComponent } from './app/greetings/greetings.component';
+import { AppComponent } from './app/app.component';
+import { isDevMode } from '@angular/core';
 
-(async () => {
-  try {
-    const app = await createApplication(appConfig);
-    const appElement = createCustomElement(GreetingsComponent, {
-      injector: app.injector,
-    });
-    customElements.define('ng-greetings-mfe', appElement);
-  } catch (err) {
-    console.error(err);
-  }
-})();
+if (isDevMode()) {
+  // Bootstrap the application only in development mode to run it locally for development and testing.
+  bootstrapApplication(AppComponent, appConfig).catch(err => console.error(err));
+} else {
+  // Create a custom element and define it as a web component
+  // This allows the application to be used as a micro frontend in other applications.
+  (async () => {
+    try {
+      const app = await createApplication(appConfig);
+      const appElement = createCustomElement(GreetingsComponent, {
+        injector: app.injector,
+      });
+      customElements.define('ng-greetings-mfe', appElement);
+    } catch (err) {
+      console.error(err);
+    }
+  })();  
+}
