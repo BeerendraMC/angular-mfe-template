67cb022014eae73e115acf782be2fdd836d0c836
Beerendra M C <beerendramc@gmail.com>
2025-08-14 13:24:13 +0530

    Update webpack.config.js
    added config to move {project_name}.html file into build destination path

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,14 @@
-const path = require("path");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-module.exports = (env) => {
+module.exports = env => {
   if (!env || !env.project_name) {
     throw new Error(
       "Error: 'project_name' environment variable is required. Please provide it using --env project_name=<your-project-name>"
     );
   }
   const projectName = env.project_name;
-  const environment = env.target_env || "dev";
+  const environment = env.target_env || 'dev';
 
   const srcPath = `./dist/${projectName}-${environment}/browser`;
   const dstPath = `./elements/${projectName}-${environment}`;
@@ -19,34 +19,39 @@ module.exports = (env) => {
   console.log(`Output Path: ${dstPath}`);
 
   return {
-    mode: "production",
+    mode: 'production',
     entry: {
-      bundle: [`${srcPath}/polyfills.js`, `${srcPath}/main.js`],
+      bundle: [`${srcPath}/polyfills.js`, `${srcPath}/main.js`]
     },
     output: {
       path: path.resolve(__dirname, dstPath), // Output directory
-      filename: `${projectName}-element.js`,
+      filename: `${projectName}-element.js`
     },
     module: {
       rules: [
         // You might need rules for handling other file types (e.g., Babel for older browser support)
-      ],
+      ]
     },
     plugins: [
       new CopyWebpackPlugin({
         patterns: [
           {
             from: `${srcPath}/styles.css`,
-            to: `${projectName}-styles.css`,
+            to: `${projectName}-styles.css`
           },
           {
             from: `${srcPath}/assets`,
-            to: "assets",
-            noErrorOnMissing: true, // Prevents errors if the assets directory does not exist
+            to: 'assets',
+            noErrorOnMissing: true // Prevents errors if the assets directory does not exist
           },
-        ],
-      }),
-    ],
+          // Comment this out if you don't want to view MFE as a standalone app
+          {
+            from: `./elements/${projectName}.html`,
+            to: `index.html`
+          }
+        ]
+      })
+    ]
     // devtool: 'inline-source-map' // Optional: for easier debugging
   };
 };
